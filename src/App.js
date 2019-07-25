acfea73f7a8e54f93c6e3f4aa6f3d4554b60c8c6
Kapil Sharma <kapilsharmajmps@gmail.com>
2019-07-26 02:56:59 +0530

    feature: unnecessary files/code removed - also modified App.js to have routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import { Switch, Redirect, Route } from 'react-router-dom';
+import Dashboard from './containers/Dashboard/Dashboard';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Switch>
+        <Route path='/' exact component={Dashboard} />
+        <Redirect to='/' />
+      </Switch>
     </div>
   );
 }
 
-export default App;
+export default connect(null)(App);
